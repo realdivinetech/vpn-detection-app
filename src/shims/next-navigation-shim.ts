81cde9d293e75ba770b5128f6c14a0c577d74fd0
realdivinetech <divinefavourdamilare2019@gmail.com>
2025-07-17 22:13:45 +0100

    `Added Next.js shim functions for navigation and routing utilities`

--- src/shims/next-navigation-shim.ts
+++ src/shims/next-navigation-shim.ts
@@ -1,3 +1,22 @@
 export function useParams() {
   return {};
 }
+
+export function useRouter() {
+  return {
+    push: () => {},
+    replace: () => {},
+    prefetch: () => Promise.resolve(),
+    pathname: '',
+    query: {},
+    asPath: '',
+  };
+}
+
+export function usePathname() {
+  return '';
+}
+
+export function useSearchParams() {
+  return new URLSearchParams();
+}
